314b539153b5e013e76e8ef99de6f1d1600246fa
Akbar Nafisa <akbarnj788@gmail.com>
2024-05-07 14:50:58 +0700

    feat: add icons

---
+++ src/components/icon/IconArrowDown.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-arrow-down"
+  >
+    <path d="M12 5v14" />
+    <path d="m19 12-7 7-7-7" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  size: {
+    type: String,
+    default: '24',
+  },
+})
+</script>
