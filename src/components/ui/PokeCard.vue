dcf36c176274e49220ccc93a3932e607bd0b351e
Akbar Nafisa <akbarnj788@gmail.com>
2024-05-07 16:17:39 +0700

    feat: init card component

---
+++ src/components/ui/PokeCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <article class="bg-white px-3 pt-3 rounded-lg border">
+    <RouterLink to="/favorite">
+      <figure
+        class="bg-gray-100 rounded-lg flex items-center justify-center h-36"
+      >
+        <img :src="$props.image" :alt="name" />
+      </figure>
+    </RouterLink>
+
+    <section class="pb-5 pt-3">
+      <div class="flex items-center justify-between">
+        <h2 class="font-bold text-xl capitalize">
+          {{ $props.name }}
+        </h2>
+        <button>
+          <IconHearth />
+        </button>
+      </div>
+      <div>
+        <ul class="flex gap-1 mt-2">
+          <BadgeTypes
+            v-for="item in types"
+            :key="item"
+            :type="item"
+          />
+        </ul>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script setup lang="ts">
+import IconHearth from '@/components/icon/IconHearth.vue'
+import BadgeTypes from './BadgeTypes.vue'
+import type { PropType } from 'vue'
+
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  favorited: {
+    type: Boolean,
+    required: true,
+  },
+  types: {
+    type: Array as PropType<Array<string>>,
+    required: true,
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ src/components/ui/PokeCardList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card-wrapper">
+    <PokeCard
+      v-for="item in props.data"
+      :id="item.id"
+      :key="item.name"
+      :favorited="item.favorited"
+      :name="item.name"
+      :image="item.image"
+      :types="item.types"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+import PokeCard from './PokeCard.vue'
+import { CardListType } from '@/utils/interface';
+
+const props = defineProps({
+  data: {
+    type: Array as PropType<Array<CardListType>>,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+.card-wrapper {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 18px;
+}
+
+@media (max-width: 1024px) {
+  .card-wrapper {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (max-width: 768px) {
+  .card-wrapper {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 12px;
+  }
+}
+
+@media (max-width: 410px) {
+  .card-wrapper {
+    grid-template-columns: repeat(1, 1fr);
+    gap: 12px;
+  }
+}
+</style>
