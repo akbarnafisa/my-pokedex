8eabdc098b2c8e01f724d6ed6d3941d262ba5df1
Akbar Nafisa <akbarnj788@gmail.com>
2024-05-07 12:41:01 +0700

    feat: init Navbar component

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav
+    class="w-full items-center p-4 bg-white border-b border-gray-200 shadow-sm"
+  >
+    <div class="max-w-5xl flex justify-between mx-auto">
+      <div class="flex items-center">
+        <Pokeball />
+        <h1 class="ml-2 font-bold text-2xl">Pokedex</h1>
+      </div>
+      <div>
+        <Button> Favorite </Button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import Pokeball from '../icon/Pokeball.vue'
+import Button from '@/components/ui/Button.vue'
+</script>
