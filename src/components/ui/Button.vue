ba6afe894a9b92f6422da46f3fe63b609ea5dfb1
Akbar Nafisa <akbarnj788@gmail.com>
2024-05-07 12:57:52 +0700

    style: update corner radius in button

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="inline-block rounded border border-emerald-600 bg-emerald-600 px-4 py-2 text-sm font-semibold text-white hover:bg-transparent hover:text-emerald-600 focus:outline-none focus:ring active:text-emerald-500 transition duration-200"
+    class="inline-block rounded-lg border border-emerald-600 bg-emerald-600 px-4 py-2 text-sm font-semibold text-white hover:bg-transparent hover:text-emerald-600 focus:outline-none focus:ring active:text-emerald-500 transition duration-200"
     href="#"
     type="button"
   >
